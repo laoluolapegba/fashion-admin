5f175b64d4a1a131fdbd404d016dc52901c16aa0
Olaoluwa Olapegba <laoluolapegba@gmail.com>
2026-03-15 23:01:25 +0000

    fix: storage params added to next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'flzpfxtdlptgjwhodxhy.supabase.co',
+                pathname: '/storage/v1/object/public/**',
+            },
+        ],
+    },
+    reactCompiler: true,
 };
 
 export default nextConfig;
